bbece5fcba8e420f8dcbad2ec33c919f5e4502d7
Christoph Weber <WeberChristoph85@gmail.com>
2023-04-12 20:08:32 +0200

    updated app.js

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-require("dotenv/config");
+// ℹ️ Gets access to environment variables/settings
+// https://www.npmjs.com/package/dotenv
+require("dotenv").config();
+
+// ℹ️ Connects to the database
 require("./db");
+
+// Handles http requests (express is node js framework)
+// https://www.npmjs.com/package/express
 const express = require("express");
-const { isAuthenticated } = require("./middleware/jwt.middleware");
 
 const app = express();
 
+// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
 // 👇 Start handling routes here
-const allRoutes = require("./routes");
-app.use("/api", allRoutes);
+app.use("/auth", require("./routes/auth.routes"));
+app.use("/api", require("./routes/index.routes"));
+app.use("/api", require("./routes/project.routes"));
+app.use("/api", require("./routes/contribution.routes"));
 
-const projectRouter = require("./routes/project.routes");
-app.use("/api", isAuthenticated, projectRouter); 
-
-const taskRouter = require("./routes/contribution.routes");
-app.use("/api", isAuthenticated, taskRouter); 
-
-const authRouter = require("./routes/auth.routes");       
-app.use("/auth", authRouter);  
-
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
 module.exports = app;

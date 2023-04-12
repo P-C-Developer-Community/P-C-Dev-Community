bab7a43e7e4cde794fa2846575b31baa1fac69d0
Christoph Weber <WeberChristoph85@gmail.com>
2023-04-12 22:08:45 +0200

    implemented contribution & project routes

--- routes/contribution.routes.js
+++ routes/contribution.routes.js
@@ -2,21 +2,20 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
-const Project = require("../models/Project.model");
 const Contribution = require("../models/Contribution.model");
 
 //  POST /api/projects  -  Creates a new project
 router.post("/contribution", (req, res, next) => {
   const { title, description } = req.body;
 
-  Project.create({ title, description })
+  Contribution.create({ title, description })
     .then((response) => res.json(response))
     .catch((err) => res.json(err));
 });
 
 //  GET /api/projects -  Retrieves all of the projects
 router.get("/contribution", (req, res, next) => {
-  Project.find()
+  Contribution.find()
     .then((allContribution) => res.json(allContributions))
     .catch((err) => res.json(err));
 });
@@ -31,7 +30,7 @@ router.get("/contributions/:contributionId", (req, res, next) => {
   }
 
 
-  Project.findById(contributionId)
+  Contribution.findById(contributionId)
     .then((project) => res.status(200).json(contribution))
     .catch((error) => res.json(error));
 });
@@ -45,7 +44,7 @@ router.put("/contributions/:contributionId", (req, res, next) => {
     return;
   }
 
-  Project.findByIdAndUpdate(contributionId, req.body, { new: true })
+  Contribution.findByIdAndUpdate(contributionId, req.body, { new: true })
     .then((updatedContribution) => res.json(updatedContribution))
     .catch((error) => res.json(error));
 });
@@ -59,7 +58,7 @@ router.delete("/contributions/:contributionId", (req, res, next) => {
     return;
   }
 
-  Project.findByIdAndRemove(contributionId)
+  Contribution.findByIdAndRemove(contributionId)
     .then(() =>
       res.json({
         message: `Project with ${contributionId} is removed successfully.`,
